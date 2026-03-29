612b7381a850ba119df0083717a75df19ade2aef
Dawood Sardar <dawoodsardar252@gmail.com>
2026-03-29 08:42:48 +0500

    Initial commit, added config/db.js and models/Users.js

---
+++ models/Users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', userschema);
