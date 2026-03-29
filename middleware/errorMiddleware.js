e10b657cb62f105c94bce2a5e8bfd9f30048d754
Dawood Sardar <dawoodsardar252@gmail.com>
2026-03-29 11:10:55 +0500

    create error handler, and put everything together in server file

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,8 @@
+// Global Error Handler
+const errorHandler = (err, req, res, next) => {
+    res.status(err.status || 500).json(
+        { message: err.message || "Server Error" }
+    )
+}
+
+module.exports = errorHandler
---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
+const errorHandler = require('./middleware/errorMiddleware')
+
+app.use(express.json())
+
+connectDB()
+
+app.use('/api/users', userRoutes)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log('Server running on Port ${PORT}')
+})
