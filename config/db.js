d32a0206535dd5bef18887e5e2f387f5917aa02a
Dawood Sardar <dawoodsardar252@gmail.com>
2026-03-29 11:39:44 +0500

    Fix Error: Exported userRoutes, and corrected mogno url variable name

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('DB Connected');
     } catch (err) {
         console.error(err.message);
